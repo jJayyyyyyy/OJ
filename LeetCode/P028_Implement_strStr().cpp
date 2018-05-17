9a821120bc73c4f52d894e50ac1886eee7d7a2a6
FrozenMap <ljjdbd123@hotmail.com>
2018-05-17 14:19:24 +0000

    update LeetCode P028

--- LeetCode/P028_Implement_strStr().cpp
+++ LeetCode/P028_Implement_strStr().cpp
@@ -9,7 +9,9 @@
 // using namespace std;
 
 // 这里用了最暴力的办法，逐个对比，每次回退, 缺点是慢
-// 还有很大的优化空间, 比如用KMP
+// 优化1, 外层for循环, 不必 [0, len1), 而是 [0, len1 - len2 + 1) 即可, 可以省去大量无效的比较
+// 因为如果超过了右侧区间, 剩下的haystack长度就不够needle了
+// 优化2, KMP
 class Solution {
 public:
 	int strStr(string haystack, string needle) {
@@ -20,15 +22,12 @@ public:
 		}else if( len1 >= len2 ){
 			for( int i = 0; i < len1; i++ ){
 				bool isMatch = true;
-				int k = i;
-				for( int j = 0; j < len2; ){
-					if( haystack[k] != needle[j] ){
+				for( int j = 0; j < len2; j++ ){
+					if( haystack[i + j] != needle[j] ){
 						isMatch = false;
 						break;
 					}
-					k++;
-					j++;
-					if( k == len1 ){
+					if( i + j == len1 ){
 						if( j != len2 ){
 							isMatch = false;
 						}
@@ -45,8 +44,31 @@ public:
 		}
 	}
 	
+	// 优化1
+	int strStr1(string haystack, string needle){
+		int len1 = haystack.size(), len2 = needle.size();
+		if( len2 == 0 ){
+			return 0;
+		}else if( len1 < len2 ){
+			return -1;
+		}else{
+			for( int i = 0; i < len1 + 1 - len2; i++ ){
+				int j = 0;
+				for( j = 0; j < len2; j++ ){
+					if( haystack[i + j] != needle[j] ){
+						break;
+					}
+				}
+				if( j == len2 ){
+					return i;
+				}
+			}
+			return -1;
+		}
+	}
+	
 	// TODO: KMP
-	int strStrKMP(string haystack, string, needle){
+	int strStrKMP(string haystack, string needle){
 		return 0;
 	}
 };
--- readme.md
+++ readme.md
@@ -2,8 +2,14 @@
 
 *	tools是备考PAT的一些tips
 
+<br><br>
+
 ##  经典题型
 
 *	最大连续子列和
 
     [LeetCode, P053_Maximum_Subarray](https://github.com/jJayyyyyyy/OJ/blob/master/LeetCode/P053_Maximum_Subarray.cpp)
+
+*   KMP 字符串匹配
+
+    [LeetCode, P028_Implement_strStr()](https://github.com/jJayyyyyyy/OJ/blob/master/LeetCode/P028_Implement_strStr().cpp)
