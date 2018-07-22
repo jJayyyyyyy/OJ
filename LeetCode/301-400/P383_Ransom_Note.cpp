d353dd5b73540f8db1bb54df450049d494285353
FrozenMap <ljjdbd123@hotmail.com>
2018-07-22 07:39:29 +0000

    add LeetCode P383

---
+++ LeetCode/301-400/P383_Ransom_Note.cpp
@@ -0,0 +1,28 @@
+/*
+https://leetcode.com/problems/ransom-note/description/
+map查找与计数
+同类题目 P383, P349, P350
+
+注意:
+*/
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	bool canConstruct(string ransomNote, string magazine) {
+		int letterCnt[128] = {0};
+		for( char ch : magazine ){
+			letterCnt[ch]++;
+		}
+
+		for( char ch : ransomNote ){
+			letterCnt[ch]--;
+			if( letterCnt[ch] < 0 ){
+				return false;
+			}
+		}
+		return true;
+	}
+};
