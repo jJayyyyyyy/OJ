667f922613bdc3751f6de725d207a22844f96008
FrozenMap <ljjdbd123@hotmail.com>
2018-07-29 04:59:12 +0000

    update LeetCode P414

--- LeetCode/401-500/P414_Third_Maximum_Number.cpp
+++ LeetCode/401-500/P414_Third_Maximum_Number.cpp
@@ -3,36 +3,42 @@ https://leetcode.com/problems/third-maximum-number/description/
 找到第三大的数
 同类题目 P414
 
+注意边界条件，可能会检测 int 的最小值
+所以要把变量的范围扩大到long long
+要使用<climits>中的 LLONG_MIN
+
+http://www.cplusplus.com/reference/climits/
 */
 
 #include <iostream>
 #include <vector>
+#include <climits>
 using namespace std;
 
 class Solution {
 public:
 	int thirdMax(vector<int>& nums) {
-		int first = INT_MIN;
-		int second = INT_MIN;
-		int third = INT_MIN;
+		long long first = LLONG_MIN;
+		long long second = LLONG_MIN;
+		long long third = LLONG_MIN;
 
 		for( int n : nums ){
 			if( n > first ){
 				third = second;
 				second = first;
 				first = n;
-			}else if( n > second ){
+			}else if( n < first && n > second ){
 				third = second;
 				second = n;
-			}else if( n > third ){
+			}else if( n < second && n > third ){
 				third = n;
 			}
 		}
 
-		if( third == INT_MIN ){
-			return first;
+		if( third == LLONG_MIN ){
+			return (int)first;
 		}else{
-			return third;
+			return (int)third;
 		}
 	}
 };
