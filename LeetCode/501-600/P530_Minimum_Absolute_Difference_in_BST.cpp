28047dbd490b8ff367978cd77b94c183a71c52de
FrozenMap <ljjdbd123@hotmail.com>
2018-09-06 01:55:39 +0000

    add LeetCode P530

---
+++ LeetCode/501-600/P530_Minimum_Absolute_Difference_in_BST.cpp
@@ -0,0 +1,34 @@
+/*
+https://leetcode.com/problems/minimum-absolute-difference-in-bst/description/
+给定BST, 求 min( abs(v[i] - v[j]) )
+同类题目 P530
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution{
+public:
+	int preVal = -1;
+	int ans = 0x3fffffff;
+
+	void inTrav(TreeNode * root){
+		if( root != NULL ){
+			inTrav(root->left);
+			if( preVal != -1 ){
+				int nowVal = root->val;
+				ans = min(ans, nowVal - preVal);
+			}
+			preVal = root->val;
+			inTrav(root->right);
+		}
+	}
+
+	// there are at least two nodes in the BST
+	int getMinimumDifference(TreeNode * root) {
+		inTrav(root);
+		return ans;
+	}
+};
+
