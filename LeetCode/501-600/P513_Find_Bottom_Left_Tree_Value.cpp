fce4f2a5292b537bcfb6d4b4b42e0c9ce57b0853
FrozenMap <ljjdbd123@hotmail.com>
2018-10-14 05:56:07 +0000

    add LeetCode P513

---
+++ LeetCode/501-600/P513_Find_Bottom_Left_Tree_Value.cpp
@@ -0,0 +1,68 @@
+/*
+https://leetcode.com/problems/find-bottom-left-tree-value/description/
+层序遍历，找到最后一行的第一个结点
+同类题目 P513, P515
+
+如果题目定义的结构体中不包含 level
+如何在层序遍历的时候判断行尾！
+
+两层循环
+while( q.size() ){
+	size_t size = q.size();
+	
+	while( size > 0 ){
+		TreeNode * node = q.front();
+		// vis q
+		q.pop();
+		size--;
+
+		if( node->left != NULL ){
+			q.push(node->left);
+		}
+		if( node->right != NULL ){
+			q.push(node->right);
+		}
+	}
+}
+
+参考
+https://leetcode.com/problems/find-bottom-left-tree-value/discuss/98784/BFS-and-DFS-Solutions
+*/
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution{
+public:
+	int findBottomLeftValue(TreeNode * root){
+		queue<TreeNode *> q;
+		q.push(root);
+		int ans = 0;
+		while( q.size() ){
+			size_t size = q.size();
+			ans = q.front()->val;
+			while( size > 0 ){
+				TreeNode * node = q.front();
+				// vis q
+				q.pop();
+				size--;
+
+				if( node->left != NULL ){
+					q.push(node->left);
+				}
+				if( node->right != NULL ){
+					q.push(node->right);
+				}
+			}
+		}
+		return ans;
+	}
+};
