7244461cc0e9882e13f5ef4678e4098a95f80144
FrozenMap <ljjdbd123@hotmail.com>
2018-10-19 10:15:17 +0000

    add LeetCode P706

---
+++ LeetCode/701-800/P706_Design_HashMap.cpp
@@ -0,0 +1,41 @@
+/*
+https://leetcode.com/problems/design-hashmap/description/
+实现 hashMap
+同类题目 P706
+
+TODO:
+学习 STL 中 unordered_map 的实现
+/usr/include/c++/4.8.4/bits/unordered_map.h
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{
+public:
+	vector<int> hasharray;
+
+	MyHashMap(){
+		hasharray = vector<int>(1000004, -1);
+	}
+
+	void put(int key, int value){
+		if( key >= 0 && key <= 1000000 ){
+			hasharray[key] = value;
+		}
+	}
+
+	int get(int key){
+		if( key >= 0 && key <= 1000000 ){
+			return hasharray[key];
+		}
+		return -1;
+	}
+
+	void remove(int key){
+		if( key >= 0 && key <= 1000000 ){
+			hasharray[key] = -1;
+		}
+	}
+};
