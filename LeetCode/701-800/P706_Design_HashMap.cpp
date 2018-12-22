03906c877afd7811506bc5e8c7556dd1e024dccc
FrozenMap <ljjdbd123@hotmail.com>
2018-12-22 10:25:18 +0000

    add LeetCode P705

--- LeetCode/701-800/P706_Design_HashMap.cpp
+++ LeetCode/701-800/P706_Design_HashMap.cpp
@@ -1,41 +1,44 @@
 /*
-https://leetcode.com/problems/design-hashmap/description/
-实现 hashMap
-同类题目 P706
+https://leetcode.com/problems/design-hashset/description/
+实现 hashSet
+同类题目 P706, P705
 
 TODO:
-学习 STL 中 unordered_map 的实现
-/usr/include/c++/4.8.4/bits/unordered_map.h
+学习 STL 中 unordered_set 的实现
+/usr/include/c++/4.8.4/bits/unordered_set.h
 */
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
-class Solution{
+class MyHashSet {
 public:
-	vector<int> hasharray;
+	vector<bool> hasharray;
+	int SIZE;
 
-	MyHashMap(){
-		hasharray = vector<int>(1000004, -1);
+	/** Initialize your data structure here. */
+	MyHashSet() {
+		hasharray = vector<bool>(1000007, false);
+		SIZE = 1000000;
 	}
-
-	void put(int key, int value){
-		if( key >= 0 && key <= 1000000 ){
-			hasharray[key] = value;
+	
+	void add(int key) {
+		if( key >= 0 && key <= SIZE ){
+			hasharray[key] = true;
 		}
 	}
-
-	int get(int key){
-		if( key >= 0 && key <= 1000000 ){
+	
+	void remove(int key) {
+		if( key >= 0 && key <= SIZE ){
+			hasharray[key] = false;
+		}
+	}
+	
+	/** Returns true if this set contains the specified element */
+	bool contains(int key) {
+		if( key >= 0 && key <= SIZE ){
 			return hasharray[key];
-		}
-		return -1;
-	}
-
-	void remove(int key){
-		if( key >= 0 && key <= 1000000 ){
-			hasharray[key] = -1;
 		}
 	}
 };
