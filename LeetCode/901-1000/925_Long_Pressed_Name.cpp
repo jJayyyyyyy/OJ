330d9f8880bb422dc610fb90de8dadf34c37c6e0
FrozenMap <ljjdbd123@hotmail.com>
2018-12-28 04:19:17 +0000

    add LeetCode P925

---
+++ LeetCode/901-1000/925_Long_Pressed_Name.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	bool isLongPressedName(string name, string typed) {
+		name.push_back('#');
+		typed.push_back('#');
+		vector<string> n, t;
+		int len1 = name.size();
+		int len2 = typed.size();
+		int pre = 0;
+		for( int i = 1; i < len1; i++ ){
+			if( name[i-1] != name[i] ){
+				int len = i - pre;
+				n.push_back(name.substr(pre, len));
+				pre = i;
+			}
+		}
+
+		pre = 0;
+		for( int i = 1; i < len2; i++ ){
+			if( typed[i-1] != typed[i] ){
+				int len = i - pre;
+				t.push_back(typed.substr(pre, len));
+				pre = i;
+			}
+		}
+
+		len1 = n.size();
+		len2 = t.size();
+		if( len1 != len2 ){
+			return false;
+		}
+		if( len1 == len2 ){
+			bool ans = true;
+			for( int i = 0; i < len1; i++ ){
+				if( n[i][0] != t[i][0] ){
+					ans = false;
+					break;
+				}else{
+					if( n[i].size() > t[i].size() ){
+						ans = false;
+						break;
+					}
+				}
+			}
+			return ans;
+		}
+	}
+};
+
+class Solution {
+public:
+	bool isLongPressedName(string name, string typed) {
+		int len1 = name.size();
+		int len2 = typed.size();
+		int i = 0, j = 0;
+		while( i < len1 && j < len2 ){
+			if( name[i] == typed[j] ){
+				i++;
+				j++;
+			}else{
+				j++;
+			}
+		}
+		return i == len1;
+	}
+};
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	Solution s;
+	string name = "alex";
+	string typed = "aaleex";
+	s.isLongPressedName(name, typed);
+
+	return 0;
+}
