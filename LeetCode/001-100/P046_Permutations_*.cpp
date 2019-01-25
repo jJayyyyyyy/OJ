92be08b7480962c18d29359cf6f7ac86c0b305d4
FrozenMap <ljjdbd123@hotmail.com>
2019-01-25 08:37:48 +0000

    add LeetCode P046

---
+++ LeetCode/001-100/P046_Permutations_*.cpp
@@ -0,0 +1,70 @@
+/*
+https://leetcode.com/problems/permutations/description/
+求全排列
+同类题目 P078, P090, P046
+
+参考思路:
+找出 f(n-1) 和 f(n) 之间的关系
+
+以 nums = [1, 2, 3] 为例
+假设 f(n-1) 是 [ [1, 2], [2, 1] ], 也就是 [1, 2] 的全排列
+那么 f(n) 就是在 f(n-1) 的基础上, 把 3 插入到每一个位置, 也就是说
+[1, 2] 会分裂成
+|--- [3, 1, 2]
+|--- [1, 3, 2]
+|--- [1, 2, 3]
+
+而 [2, 1] 会分裂成
+|--- [3, 2, 1]
+|--- [2, 3, 1]
+|--- [2, 1, 3]
+
+因此可以得到 f(n) = [ [3, 1, 2], [1, 3, 2], [1, 2, 3], [3, 2, 1], [2, 3, 1], [2, 1, 3] ]
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// io加速
+static const auto io_speed_up = []() {
+	std::ios::sync_with_stdio(false);
+	cin.tie(0);
+	return 0;
+} ();
+
+class Solution {
+public:
+	vector<vector<int>> permute(vector<int>& nums) {
+		vector< vector<int> > ans, tmpans;
+		int len = nums.size();
+		if( len > 1 ){
+			ans.push_back(vector<int>(1, nums[0]));
+			for( int i = 1; i < len; i++ ){
+				int num = nums[i];
+				for( vector<int> line : ans ){
+					int len2 = line.size();
+					for( int i = 0; i <= len2; i++ ){
+						vector<int> tmpline = line;
+						tmpline.insert(tmpline.begin() + i, num);
+						tmpans.push_back(tmpline);
+					}
+				}
+				ans = tmpans;
+				tmpans.clear();
+			}
+		}
+		return ans;
+	}
+};
+
+int main() {
+	vector<int> nums(3);
+	nums[0] = 1;
+	nums[1] = 2;
+	nums[2] = 3;
+
+	Solution s;
+	s.permute(nums);
+	return 0;
+}
