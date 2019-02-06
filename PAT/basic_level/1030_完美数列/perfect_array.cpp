eb2845e08d3a09262d0a139c144d3fad717872a6
FrozenMap <ljjdbd123@hotmail.com>
2019-02-06 15:23:51 +0000

    update PAT_B_1030

--- PAT/basic_level/1030_完美数列/perfect_array.cpp
+++ PAT/basic_level/1030_完美数列/perfect_array.cpp
@@ -6,6 +6,8 @@ using namespace std;
 long long num[MAXSIZE]={0};
 
 int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 	int n, i, maxCnt=1, iMin, iMax;;
 	long long p, mp;
 	cin>>n>>p;
@@ -24,3 +26,52 @@ int main(){
 	cout<<maxCnt<<'\n';
 	return 0;
 }
+
+
+/*
+Solution 2
+*/
+
+/*
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int len;
+	long long p;
+	cin >> len >> p;
+
+	vector<int> numList(len);
+
+	for( int i = 0; i < len; i++ )
+	{
+		cin>>numList[i];
+	}
+
+	sort(numList.begin(), numList.end());
+	long long mp;
+	int maxGap=1, gap, left=0, right=1;
+
+	for( right = 1; right < len; right++ )
+	{
+		mp = numList[left] * p;
+		if( numList[right] > mp )
+		{
+			left++;
+		}
+		else
+		{
+			gap = right - left + 1;
+			maxGap = maxGap > gap ? maxGap : gap;
+		}
+	}
+
+	cout << maxGap << '\n';
+	return 0;
+}
+
+*/
