ca3394785a57eea9f47b0ebdc1ba88c7075201d6
FrozenMap <ljjdbd123@hotmail.com>
2019-02-08 02:08:55 +0000

    update PAT_A_1154

--- PAT/advanced_level/1154_Vertex_Coloring/1154_Vertex_Coloring.cpp
+++ PAT/advanced_level/1154_Vertex_Coloring/1154_Vertex_Coloring.cpp
@@ -7,58 +7,80 @@ https://pintia.cn/problem-sets/994805342720868352/problems/1071785301894295552
 2. 如果返回的是 true, 再遍历所有结点, 将其 color 输入一个 set<int> s
    最后得到的 s.size() 就是 k-coloring 中的 k
 */
+
 #include <iostream>
-#include <set>
 #include <vector>
-#include <algorithm>
+#include <unordered_set>
 using namespace std;
 
-bool valid;
 vector< vector<int> > graph;
-vector<int> color;
+vector<int> colorList;
 
-void bfs(int n){
-	for( int i = 0; i < n; i++ ){
-		for( int j : graph[i] ){
-			if( color[i] == color[j] ){
+// 检查相邻顶点颜色是否相同
+// BFS
+bool checkValid(int n)
+{
+	int valid = true;
+	for( int now = 0; now < n; now++ )
+	{
+		vector<int> neighbor = graph[now];
+		for( int next : neighbor )
+		{
+			if( colorList[now] == colorList[next] )
+			{
 				valid = false;
-				return;
+				break;
 			}
 		}
 	}
+	return valid;
 }
 
-int main(){
+int main()
+{
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
-	int n, m, k, v1, v2;
-	cin>>n>>m;
+	int n, m;
+	cin >> n >> m;
+
 	graph.resize(n);
-	color.resize(n);
-	for( int i = 0; i < m; i++ ){
-		cin>>v1>>v2;
+	colorList.resize(n);
+
+	int v1, v2;
+	for( int i = 0; i < m; i++ )
+	{
+		cin >> v1 >> v2;
 		graph[v1].push_back(v2);
-		// 下面这行注释掉也可以, 也就是检查完 graph[v1][v2] 后不用重复检查 graph[v2][v1]
-		// graph[v2].push_back(v1);
+		graph[v2].push_back(v1);
 	}
 
-	cin>>k;
-	for( int i = 0; i < k; i++ ){
-		for( int v = 0; v < n; v++ ){
-			cin>>color[v];
+	int k;
+	cin >> k;
+	for(int qeury = 0; qeury < k; qeury++ )
+	{
+		for( int v = 0; v < n; v++ )
+		{
+			cin >> colorList[v];
 		}
-		valid = true;
-		bfs(n);
-		if(valid == true){
-			set<int> s;
-			for( int v = 0; v < n; v++ ){
-				s.insert(color[v]);
+
+		bool valid = checkValid(n);
+		if( valid == true )
+		{
+			// cntColor
+			unordered_set<int> s;
+			for( int color : colorList )
+			{
+				s.insert(color);
 			}
-			cout<<s.size()<<"-coloring\n";
-		}else{
-			cout<<"No\n";
+			int cnt = s.size();
+			cout << cnt << "-coloring\n";
 		}
+		else
+		{
+			cout << "No\n";
+		}
+
 	}
 
 	return 0;
