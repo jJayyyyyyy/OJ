affc99352b61a82a3b695cbbfba8007bc67919a8
FrozenMap <ljjdbd123@hotmail.com>
2019-02-10 13:08:08 +0000

    update PAT_A_1134

--- PAT/advanced_level/1134_Vertex_Cover/vertex_cover.cpp
+++ PAT/advanced_level/1134_Vertex_Cover/vertex_cover.cpp
@@ -1,60 +1,75 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#define MAXSIZE 10004
+#include <algorithm>
 using namespace std;
 
-vector<int> edgeOfV[MAXSIZE];
-bool visE[MAXSIZE];
+vector< vector<int> > edge_of_vertex;
+vector<bool> visited_edge;
 
-void init(){
-	fill(visE, visE + MAXSIZE, false);
+inline void check(int m)
+{
+	bool ans = true;
+	for( int edge : visited_edge )
+	{
+		// 如果还存在没有没有被访问过的边，说明不能 cover
+		if( edge == false )
+		{
+			ans = false;
+			break;
+		}
+	}
+	if( ans == true )
+	{
+		cout << "Yes\n";
+	}
+	else
+	{
+		cout << "No\n";
+	}
 }
 
-int main(){
+int main()
+{
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	
+
 	int n, m;
-	cin>>n>>m;
-	
+	cin >> n >> m;
+	edge_of_vertex.resize(n);
+	visited_edge.resize(m);
+
+	// vertex_id
 	int v1, v2;
-	for( int eID = 0; eID < m; ++eID ){
-		cin>>v1>>v2;
-		
-		// 给每条边编号, [0, m-1]
-		edgeOfV[v1].push_back(eID);	// 保存与节点v1相连的边
-		edgeOfV[v2].push_back(eID);
+	// 给每条边编号, eid = [0, m-1]
+	for( int eid = 0; eid < m; eid++ )
+	{
+		cin >> v1 >> v2;
+		// 保存与顶点 v1 相连的边
+		edge_of_vertex[v1].push_back( eid );
+		edge_of_vertex[v2].push_back( eid );
 	}
-	
-	int cntQuery, k, v3;
-	cin>>cntQuery;
-	for( int q = 0; q < cntQuery; q++ ){
-		// 初始化，使每条边的状态为未访问
-		init();
-		
-		cin>>k;
-		for( int i = 0; i < k; i++ ){
-			cin>>v3;
-			for( auto eID : edgeOfV[v3] ){
-				// 标记与顶点vid相连的每条边eid
-				visE[eID] = true;
+
+	int k;
+	cin >> k;
+	for( int query = 0; query < k; query++ )
+	{
+		// 初始化, 使每条边的状态为未访问
+		fill(visited_edge.begin(), visited_edge.end(), false);
+		int ttl;
+		cin >> ttl;
+		for( int i = 0; i < ttl; i++ )
+		{
+			int v;
+			cin >> v;
+			for( int eid : edge_of_vertex[v] )
+			{
+				// 标记与顶点 v 相连的每条边 eid
+				visited_edge[eid] = true;
 			}
 		}
-		
-		bool isCover = true;
-		for( int eID = 0; eID < m; eID++ ){
-			// 如果还存在没有没有被访问过的边，说明不能cover
-			if( visE[eID] == false ){
-				isCover = false;
-				break;
-			}
-		}
-		if( isCover == true ){
-			cout<<"Yes\n";
-		}else{
-			cout<<"No\n";
-		}
+
+		check(m);
 	}
+
 	return 0;
 }
