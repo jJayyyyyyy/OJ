9eb7b10fb187a9e51f92cceea36c076bbdc4e2d3
FrozenMap <ljjdbd123@hotmail.com>
2019-02-16 13:44:11 +0000

    update PAT_A_1003

--- PAT/advanced_level/1003_Emergency/emergency.cpp
+++ PAT/advanced_level/1003_Emergency/emergency.cpp
@@ -1,3 +1,8 @@
+/*
+求 src 到 dest 的最短路径, 如果最短路径不唯一, 则求其中路径结点权值之和最大的路径
+同类题目 1003, 1018, 1030, 1072, 1087, 1111
+*/
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -6,94 +11,118 @@
 using namespace std;
 
 int n, m, src, dest;
+int max_cost=0;
+int cnt_path=0;
 bool vis[MAXSIZE] = {false};
-int G[MAXSIZE][MAXSIZE];
-int dist[MAXSIZE];
+int graph[MAXSIZE][MAXSIZE];
+int dist[MAXSIZE];	// dist[ix] == dist from src to ix
 int team[MAXSIZE];
-int maxCost=0, cntPath=0;
-vector<int> pre[MAXSIZE], tmpPath, ansPath;
+vector<int> pre_list[MAXSIZE], tmp_path, ans_path;
 
-void init(){
-	fill(G[0], G[0] + MAXSIZE*MAXSIZE, INF);
+void init_graph_and_dist()
+{
 	fill(dist, dist + MAXSIZE, INF);
+	fill(graph[0], graph[0] + MAXSIZE * MAXSIZE, INF);
 }
 
-void Dijkstra(){
+void Dijkstra()
+{
 	dist[src] = 0;
-	while( vis[dest] == false ){
+	while( vis[dest] == false )
+	{
 		int mid = -1;
 		int minDist = INF;
-		for( int i = 0 ; i < n; i++ ){
-			if( vis[i] == false && dist[i] < minDist ){
+		for( int i = 0; i < n; i++ )
+		{
+			if( vis[i] == false && dist[i] < minDist )
+			{
 				mid = i;
 				minDist = dist[i];
 			}
 		}
-		if( mid == -1 ){
-			return;
+
+		if( mid == -1 )
+		{
+			return;	// dest 无法到达
 		}
 
 		vis[mid] = true;
-		for( int i = 0; i < n; i++ ){
-			if( vis[i] == false && G[mid][i] != INF ){
-				if( dist[mid] + G[mid][i] < dist[i] ){
-					dist[i] = dist[mid] + G[mid][i];
-					pre[i].clear();
-					pre[i].push_back(mid);
-				}else if( dist[mid] + G[mid][i] == dist[i] ){
-					pre[i].push_back(mid);
+		for( int i = 0; i < n; i++ )
+		{
+			if( vis[i] == false && graph[mid][i] != INF )
+			{
+				if( dist[mid] + graph[mid][i] < dist[i] )
+				{
+					dist[i] = dist[mid] + graph[mid][i];
+					pre_list[i].clear();
+					pre_list[i].push_back(mid);
+				}
+				else if( dist[mid] + graph[mid][i] == dist[i] )
+				{
+					pre_list[i].push_back(mid);
 				}
 			}
 		}
 	}
 }
 
-void calCost(){
-	int tmpCost = 0;
-	for( int i = tmpPath.size()-1; i>=0; i-- ){
-		int now = tmpPath[i];
-		// int next = tmpPath[i-1];
-		tmpCost += team[now];
+void calc_cost()
+{
+	int tmp_cost = 0;
+	for( int i = tmp_path.size() - 1; i >= 0; i-- )
+	{
+		int now = tmp_path[i];
+		// int next = tmp_path[i-1];
+		tmp_cost += team[now];
 	}
-	if( tmpCost > maxCost ){
-		maxCost = tmpCost;
-		ansPath = tmpPath;
+	if( tmp_cost > max_cost )
+	{
+		max_cost = tmp_cost;
+		ans_path = tmp_path;
 	}
 }
 
-void DFS(int v){
-	if( v == src ){
-		tmpPath.push_back(v);
-		calCost();
-		cntPath++;
-		tmpPath.pop_back();
+void DFS(int now)
+{
+	if( now == src )
+	{
+		tmp_path.push_back(now);
+		calc_cost();
+		tmp_path.pop_back();
+		cnt_path++;
 		return;
 	}
-	tmpPath.push_back(v);
-	for( int i = 0; i < pre[v].size(); i++ ){
-		DFS(pre[v][i]);
+
+	tmp_path.push_back(now);
+	for( int pre : pre_list[now] )
+	{
+		DFS(pre);
 	}
-	tmpPath.pop_back();
+	tmp_path.pop_back();
 }
 
-int main(){
+int main()
+{
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
-	init();
-	int v1, v2, len;
-	cin>>n>>m>>src>>dest;
-	for( int i = 0; i < n; i++ ){
-		cin>>team[i];
+	cin >> n >> m >> src >> dest;
+	for( int i = 0; i < n; i++ )
+	{
+		cin >> team[i];
 	}
-	for( int i = 0; i < m; i++ ){
-		cin>>v1>>v2>>len;
-		G[v1][v2] = G[v2][v1] = len;
+
+	init_graph_and_dist();
+	int v1, v2, weight;
+	for( int i = 0; i < m; i++ )
+	{
+		cin >> v1 >> v2 >> weight;
+		graph[v1][v2] = graph[v2][v1] = weight;
 	}
 
 	Dijkstra();
 	DFS(dest);
+	cout << cnt_path << ' ' << max_cost << '\n';
 
-	cout<<cntPath<<' '<<maxCost<<'\n';
 	return 0;
 }
