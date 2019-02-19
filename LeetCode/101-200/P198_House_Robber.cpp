4ecea8d5f5316fd74f09f27eb9bf12c3bdac6d1f
FrozenMap <ljjdbd123@hotmail.com>
2019-02-19 15:37:22 +0000

    update LeetCode P198

--- LeetCode/101-200/P198_House_Robber.cpp
+++ LeetCode/101-200/P198_House_Robber.cpp
@@ -56,22 +56,31 @@ public:
 
 class Solution2{
 public:
-	int rob(vector<int> & nums){
+	int rob(vector<int> & nums)
+	{
 		int len = nums.size();
-		if( len == 0 ){
+		if( len == 0 )
+		{
 			return 0;
-		}else if( len == 1 ){
+		}
+		else if( len == 1 )
+		{
 			return nums[0];
-		}else if( len == 2 ){
-			return max(nums[0], nums[1]);
-		}else{
+		}
+		else if( len == 2 )
+		{
+			return max( nums[0], nums[1] );
+		}
+		else
+		{
 			vector<int> f(len, 0);
 			f[0] = nums[0];
-			f[1] = max(nums[0], nums[1]);
-			for( int i = 2; i < len; i++ ){
-				f[i] = max( f[i-2] + nums[i], f[i-1] );
+			f[1] = max( nums[0], nums[1] );
+			for( int i = 2; i < len; i++ )
+			{
+				f[i] = max( f[i - 2] + nums[i], f[i - 1] );
 			}
-			return f[len-1];
+			return f[len - 1];
 		}
 	}
 };
