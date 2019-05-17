fd90015c7a725ec62af1a08f6f97d1a5a3af8212
FrozenMap <ljjdbd123@hotmail.com>
2019-05-17 08:57:43 +0000

    add LeetCode P015 threeSum

--- LeetCode/001-100/P015_3Sum_*.cpp
+++ LeetCode/001-100/P015_3Sum_*.cpp
@@ -1,60 +1,57 @@
 /*
- * https://leetcode.com/problems/valid-parentheses/description/
- * 括号匹配
- * stack, 栈的使用
+https://leetcode.com/problems/3sum/description/
+三个数的和为0，找出所有不重复的组合
+
+参考思路
+https://leetcode.com/problems/3sum/discuss/7402/Share-my-AC-C++-solution-around-50ms-O(N*N)-with-explanation-and-comments
+
+同类题目: P001, P018, P015, P454
 */
 
-// #include <iostream>
-// #include <vector>
-// #include <stack>
-// using namespace std;
+#include <iostream>
+#include <vector>
+using namespace std;
 
-class Solution {
+class Solution{
 public:
-	bool isValid(string s) {
-		stack<char> stk;
-		for( auto ch : s ){
-			if( ch == ')' ){
-				if( stk.size() == 0 || stk.top() != '(' ){
-					return false;
-				}else{
-					stk.pop();
+	vector< vector<int> > threeSum(vector<int> &numlist) {
+		vector< vector<int> > ans;
+		sort(numlist.begin(), numlist.end());
+		int size = numlist.size();
+		for( int i = 0; i < size; i++ ){
+			int target = -numlist[i];
+			int ileft = i + 1;
+			int iright = size - 1;
+			while( ileft < iright ){
+				int valleft = numlist[ileft];
+				int valright = numlist[iright];
+				int sum = valleft + valright;
+				if( sum < target ){
+					ileft++;
 				}
-			}else if( ch == ']' ){
-				if( stk.size() == 0 || stk.top() != '[' ){
-					return false;
-				}else{
-					stk.pop();
+				else if( sum > target ){
+					iright--;
 				}
-			}else if( ch == '}' ){
-				if( stk.size() == 0 || stk.top() != '{' ){
-					return false;
-				}else{
-					stk.pop();
+				else{
+					vector<int> line(3);
+					line[0] = -target;
+					line[1] = valleft;
+					line[2] = valright;
+					ans.push_back(line);
+
+					while( ileft < iright && numlist[ileft] == valleft ){
+						ileft++;
+					}
+
+					while( ileft < iright && numlist[iright] == valright ){
+						iright--;
+					}
 				}
-			}else{
-				stk.push(ch);
+			}
+			while( i + 1 < size && numlist[i + 1] == -target ){
+				i++;
 			}
 		}
-
-		if( stk.size() != 0 ){
-			return false;
-		}else{
-			return true;
-		}
+		return ans;
 	}
 };
-
-// int main(){
-// 	ios::sync_with_stdio(false);
-// 	cin.tie(0);
-
-// 	Solution s;
-// 	cout<<s.isValid("]")<<'\n';
-// 	cout<<s.isValid("()")<<'\n';
-// 	cout<<s.isValid("()[]{}")<<'\n';
-// 	cout<<s.isValid("(]")<<'\n';
-// 	cout<<s.isValid("([)]")<<'\n';
-// 	cout<<s.isValid("{[]}")<<'\n';
-// 	return 0;
-// }
