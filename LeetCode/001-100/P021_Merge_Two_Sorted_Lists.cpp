361b91081a8389f0741196604c6ae02096a4ffb5
FrozenMap <ljjdbd123@hotmail.com>
2019-05-19 15:34:17 +0000

    update LeetCode P021_Merge_Two_Sorted_Lists

--- LeetCode/001-100/P021_Merge_Two_Sorted_Lists.cpp
+++ LeetCode/001-100/P021_Merge_Two_Sorted_Lists.cpp
@@ -1,95 +1,79 @@
 /*
- * https://leetcode.com/problems/merge-two-sorted-lists/
- * 指针形式的归并排序
- *
- * Example
- * Input: 1->2->4, 1->3->4
- * Output: 1->1->2->3->4->4
- *
- * 注意1: 指针可能为空
- * 注意2: 凡是指针, 一定记得初始化
+https://leetcode.com/problems/merge-two-sorted-lists/
+指针形式的归并排序
+同类题目
+P021, P023
+
+注意1: 指针可能为空
+注意2: 凡是指针, 一定记得初始化
+
+参考思路
+https://leetcode.com/problems/merge-two-sorted-lists/discuss/9714/14-line-clean-C++-Solution
+先设置一个 dummyhead, 把 l1 或 l2 为空的情况合并成一种
 */
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
+struct ListNode{
+	int val;
+	ListNode * next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
 
 class Solution {
 public:
-	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-		if( l1 == NULL && l2 == NULL ){
-			return NULL;
-		}else if( l1 == NULL ){
-			return l2;
-		}else if( l2 == NULL ){
-			return l1;
-		}else{
-			// l1 != NULL && l2 != NULL
-			ListNode *head = NULL;
-			ListNode *p = NULL;
+	ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+		ListNode dummyhead(INT_MIN);
+		ListNode * tail = & dummyhead;
 
+		while( l1 != NULL && l2 != NULL ) {
 			if( l1->val < l2->val ){
-				head = l1;
+				tail->next = l1;
 				l1 = l1->next;
-			}else{
-				head = l2;
+			} else {
+				tail->next = l2;
 				l2 = l2->next;
 			}
-
-			p = head;
-			while( l1 && l2 ){
-				if( l1->val < l2->val ){
-					p->next = l1;
-					l1 = l1->next;
-				}else{
-					p->next = l2;
-					l2 = l2->next;
-				}
-				p = p->next;
-			}
-
-			while( l1 ){
-				p->next = l1;
-				l1 = l1->next;
-				p = p->next;
-			}
-
-			while( l2 ){
-				p->next = l2;
-				l2 = l2->next;
-				p = p->next;
-			}
-
-			return head;
+			tail = tail->next;
 		}
+
+		if( l1 != NULL ){
+			tail->next = l1;
+		}
+		else{
+			tail->next = l2;
+		}
+		tail->next = l1 ? l1 : l2;
+		return dummyhead.next;
 	}
 };
 
-// int main(){
-// 	ios::sync_with_stdio(false);
-// 	cin.tie(0);
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 
-// 	/* case1 */
-// 	ListNode *l1 = NULL, *l2 = NULL;
+	/* case1 */
+	ListNode *l1 = NULL, *l2 = NULL;
 
 
-// 	/* case2
-// 	ListNode *l1 = new ListNode(1);
-// 	l1->next = new ListNode(2);
-// 	l1->next->next = new ListNode(4);
+	/* case2
+	ListNode *l1 = new ListNode(1);
+	l1->next = new ListNode(2);
+	l1->next->next = new ListNode(4);
 
-// 	ListNode *l2 = new ListNode(1);
-// 	l2->next = new ListNode(3);
-// 	l2->next->next = new ListNode(4);
-// 	/**/
+	ListNode *l2 = new ListNode(1);
+	l2->next = new ListNode(3);
+	l2->next->next = new ListNode(4);
+	/**/
 
-// 	Solution s;
-// 	ListNode *head = s.mergeTwoLists(l1, l2);
+	Solution s;
+	ListNode *head = s.mergeTwoLists(l1, l2);
 
-// 	while(head){
-// 		cout<<head->val<<' ';
-// 		head = head->next;
-// 	}
+	while(head){
+		cout<<head->val<<' ';
+		head = head->next;
+	}
 
-// 	return 0;
-// }
+	return 0;
+}
