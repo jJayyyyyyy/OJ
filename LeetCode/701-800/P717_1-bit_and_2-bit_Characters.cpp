fef677bf4fc8dc1479a031ea7643c37258ba5d0e
Steve <ljjdbd123@hotmail.com>
2019-08-17 12:43:33 +0000

    update LeetCode P717

--- LeetCode/701-800/P717_1-bit_and_2-bit_Characters.cpp
+++ LeetCode/701-800/P717_1-bit_and_2-bit_Characters.cpp
@@ -43,3 +43,31 @@ public:
 		return i == end;
 	}
 };
+
+/*
+解法2
+从后往前
+
+We don't need to traverse the whole array, just check the last part of it.
+
+1. if there is only one symbol in array the answer is always true (as last element is 0)
+2. if there are two 0s at the end again the answer is true no matter what the rest symbols are( ...1100, ...1000,)
+3. if there is 1 right before the last element(...10), the outcome depends on the count of sequential 1, i.e.
+	a) if there is odd amount of 1(10, ...01110, etc) the answer is false as there is a single 1 without pair
+	b) if it's even (110, ...011110, etc) the answer is true, as 0 at the end doesn't have anything to pair with
+
+https://leetcode.com/problems/1-bit-and-2-bit-characters/solution/
+https://leetcode.com/problems/1-bit-and-2-bit-characters/discuss/108967/JAVA-check-only-the-end-of-array
+*/
+
+class Solution2 {
+	public boolean isOneBitCharacter(int[] bits) {
+		int ones = 0;
+		//Starting from one but last, as last one is always 0.
+		for (int i = bits.length - 2; i >= 0 && bits[i] != 0 ; i--) { 
+			ones++;
+		}
+		if (ones % 2 > 0) return false; 
+		return true;
+	}
+};
