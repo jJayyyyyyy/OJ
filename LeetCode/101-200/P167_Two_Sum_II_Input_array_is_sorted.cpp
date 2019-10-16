49e49e8328ef8a38862197c361c30ef776db343e
Steve <ljjdbd123@hotmail.com>
2019-10-16 12:33:33 +0000

    update LeetCode P167

--- LeetCode/101-200/P167_Two_Sum_II_Input_array_is_sorted.cpp
+++ LeetCode/101-200/P167_Two_Sum_II_Input_array_is_sorted.cpp
@@ -1,44 +1,48 @@
 /*
- * https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
- * 给定一个有序数组 numbers, 以及一个目标值 target, 问 numbers 里面的某两个元素之和能否等于 target
- * 同类题目 P167
- * 
- * 
- * 思路: 左右夹逼
- * 注意, 题目的测试输入保证有 Solution
+https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/?goto=old
+
+双指针 同类题目 P167, P633, P345, P680, P088, P141, P524, 
+
+输入：数组 numbers[] 已排序（升序），target
+过程：找 x, y，使得 x + y = target
+输出：[i, j]，其中 i, j 分别是 x, y 的下标（从1开始），即 numbers[i-1] = x, numbers[j-1]=y
+注：题目保证有且仅有一个答案
+
+思路: 双指针左右夹逼
+
 */
 
-// #include <iostream>
-// #include <vector>
-// #include <algorithm>
-// using namespace std;
+#include <iostream>
+#include <vector>
+using namespace std;
 
 // io加速
-// static const auto io_speed_up = []() {
-// 	std::ios::sync_with_stdio(false);
-// 	cin.tie(0);
-// 	return 0;
-// } ();
+static const auto io_speed_up = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);
+    return 0;
+} ();
 
 class Solution {
 public:
-	// assume that each input has an answer, not null
-	vector<int> twoSum(vector<int>& numbers, int target) {
-		int left=0, right=numbers.size()-1;
-		while( true ){
-			int temp = numbers[left] + numbers[right];
-			if( temp == target ){
-				break;
-			}else if( temp < target ){
-				left++;
-			}else{
-				right--;
-			}
-		}
-
-		vector<int> ans;
-		ans.push_back(left+1);
-		ans.push_back(right+1);
-		return ans;
-	}
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int len = numbers.size();
+        int i = 0, j = len - 1;
+        while ( i != j ) {
+            int x = numbers[i];
+            int y = numbers[j];
+            int sum = x + y;
+            if ( sum > target ) {
+                j--;
+            }
+            else if ( sum < target ) {
+                i++;
+            }
+            else {
+                break;
+            }
+        }
+        vector<int> ans = {i + 1, j + 1};
+        return ans;
+    }
 };
