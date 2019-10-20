daa670c0fa8c8dc44de47fcb9420394fe1457419
Steve <ljjdbd123@hotmail.com>
2019-10-20 12:33:33 +0000

    add LeetCode P021

--- LeetCode/001-100/P021_Merge_Two_Sorted_Lists_+.cpp
+++ LeetCode/001-100/P021_Merge_Two_Sorted_Lists_+.cpp
@@ -1,8 +1,9 @@
 /*
-https://leetcode.com/problems/merge-two-sorted-lists/
+https://leetcode.com/problems/merge-two-sorted-lists/?goto=old
+
 指针形式的归并排序
-同类题目
-P021, P023
+
+同类题目: P021, P023, P088, P986
 
 注意1: 指针可能为空
 注意2: 凡是指针, 一定记得初始化
@@ -16,58 +17,65 @@ https://leetcode.com/problems/merge-two-sorted-lists/discuss/9714/14-line-clean-
 using namespace std;
 
 struct ListNode{
-	int val;
-	ListNode * next;
-	ListNode(int x) : val(x), next(NULL) {}
+    int val;
+    ListNode * next;
+    ListNode(int x) : val(x), next(NULL) {}
 };
 
 class Solution {
 public:
-	ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-		ListNode dummyhead(INT_MIN);
-		ListNode * tail = & dummyhead;
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        // 伪头部，使操作统一
+        ListNode dummyhead(INT_MIN);
+        ListNode * tail = & dummyhead;
 
-		while( l1 != NULL && l2 != NULL ) {
-			if( l1->val < l2->val ){
-				tail->next = l1;
-				l1 = l1->next;
-			} else {
-				tail->next = l2;
-				l2 = l2->next;
-			}
-			tail = tail->next;
-		}
+        while( l1 != NULL && l2 != NULL ) {
+            if( l1->val < l2->val ){
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
 
-		tail->next = l1 ? l1 : l2;
-		return dummyhead.next;
-	}
+        if ( NULL != l1 ) {
+            tail->next = l1;
+        }
+        else {
+            tail->next = l2;
+        }
+        ListNode head = dummyhead.next;
+        return head;
+    }
 };
 
 int main(){
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-	/* case1 */
-	ListNode *l1 = NULL, *l2 = NULL;
+    /* case1 */
+    ListNode *l1 = NULL, *l2 = NULL;
 
 
-	/* case2
-	ListNode *l1 = new ListNode(1);
-	l1->next = new ListNode(2);
-	l1->next->next = new ListNode(4);
+    /* case2
+    ListNode *l1 = new ListNode(1);
+    l1->next = new ListNode(2);
+    l1->next->next = new ListNode(4);
 
-	ListNode *l2 = new ListNode(1);
-	l2->next = new ListNode(3);
-	l2->next->next = new ListNode(4);
-	/**/
+    ListNode *l2 = new ListNode(1);
+    l2->next = new ListNode(3);
+    l2->next->next = new ListNode(4);
+    /**/
 
-	Solution s;
-	ListNode *head = s.mergeTwoLists(l1, l2);
+    Solution s;
+    ListNode *head = s.mergeTwoLists(l1, l2);
 
-	while(head){
-		cout<<head->val<<' ';
-		head = head->next;
-	}
+    while(head){
+        cout<<head->val<<' ';
+        head = head->next;
+    }
 
-	return 0;
+    return 0;
 }
