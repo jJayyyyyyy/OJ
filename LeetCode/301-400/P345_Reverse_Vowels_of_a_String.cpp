77a67bfd08658a076be302c2ca96ef5bcb68bcaf
Steve <ljjdbd123@hotmail.com>
2019-10-25 12:33:33 +0000

    update LeetCode P345

--- LeetCode/301-400/P345_Reverse_Vowels_of_a_String.cpp
+++ LeetCode/301-400/P345_Reverse_Vowels_of_a_String.cpp
@@ -13,43 +13,42 @@ using namespace std;
 
 class Solution {
 public:
-	string reverseVowels(string s) {
-		bool vs[128] = {false};
-		vs['a'] = true;
-		vs['A'] = true;
-		vs['e'] = true;
-		vs['E'] = true;
-		vs['i'] = true;
-		vs['I'] = true;
-		vs['o'] = true;
-		vs['O'] = true;
-		vs['u'] = true;
-		vs['U'] = true;
+    string reverseVowels(string s) {
+        bool vowel_set[128] = {false};
+        vowel_set['a'] = true;
+        vowel_set['A'] = true;
+        vowel_set['e'] = true;
+        vowel_set['E'] = true;
+        vowel_set['i'] = true;
+        vowel_set['I'] = true;
+        vowel_set['o'] = true;
+        vowel_set['O'] = true;
+        vowel_set['u'] = true;
+        vowel_set['U'] = true;
 
-		int i = 0, j = s.size() - 1;
-		while( i < j ){
-
-			if( vs[ s[i] ] == false ){
-				i++;
-			}
-			if( vs[ s[j] ] == false ){
-				j--;
-			}
-			if( vs[s[i]] == true && vs[ s[j] ] == true ){
-				swap(s[i], s[j]);
-				i++;
-				j--;
-			}
-		}
-		return s;
-	}
+        int i = 0, j = s.size() - 1;
+        while( i < j ){
+            if( vowel_set[ s[i] ] == false ){
+                i++;
+            }
+            if( vowel_set[ s[j] ] == false ){
+                j--;
+            }
+            if( i < j && vowel_set[ s[i] ] == true && vowel_set[ s[j] ] == true ){
+                swap(s[i], s[j]);
+                i++;
+                j--;
+            }
+        }
+        return s;
+    }
 };
 
 
 int main(){
-	Solution s;
-	cout<<s.reverseVowels("hello")<<'\n';
-	cout<<s.reverseVowels("aA")<<'\n';	// 注意大小写
+    Solution s;
+    cout<<s.reverseVowels("hello")<<'\n';
+    cout<<s.reverseVowels("aA")<<'\n';  // 注意大小写
 
-	return 0;
+    return 0;
 }
