2678c4374f9e8163b54b4b7a692229d75f01a32d
Steve <ljjdbd123@hotmail.com>
2019-10-26 12:33:33 +0000

    update LeetCode P680

--- LeetCode/601-700/P680_Valid_Palindrome_II.cpp
+++ LeetCode/601-700/P680_Valid_Palindrome_II.cpp
@@ -1,92 +1,50 @@
 /*
 https://leetcode.com/problems/valid-palindrome-ii/description/
 判断一个字符串是否为对称的字符串
-本题最多允许一个非对称字符, 如 abca 在本题条件下是合法的对称字符串
-同类题目 P680
+本题允许删除最多一个字符，如果删除后得到的字符串还是对称的，那么认为原字符串也符合对称要求
+如 abca 在本题条件下是合法的对称字符串
+同类题目 P680, P167, P345
+
 */
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-
 /*
 参考思路
 https://leetcode.com/problems/valid-palindrome-ii/solution/
 */
 class Solution {
+private:
+    // 绝对回文
+    bool isPalindrome(const string & s, int i, int j){
+        while( i < j ){
+            if( s[i] != s[j] ){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
 public:
-	bool subStrValidPalid(string & s, int i, int j){
-		while( i < j ){
-			if( s[i] != s[j] ){
-				return false;
-			}
-			i++;
-			j--;
-		}
-		return true;
-	}
+    // 容忍一个字符的回文
+    bool validPalindrome(string s) {
+        int len = s.size();
 
-	bool validPalindrome(string s) {
-		int len = s.size();
+        if( len <= 1 ){
+            return true;
+        }
 
-		if( len <= 1 ){
-			return true;
-		}
-
-		int halflen = len / 2;
-		// 由于最多只有一个错误字符
-		for( int i = 0; i < halflen; i++ ){
-			if( s[i] != s[len-1-i] ){
-				int j = len - 1 - i;
-				return ( subStrValidPalid(s, i+1, j) || subStrValidPalid(s, i, j-1) );
-			}
-		}
-
-		return true;
-	}
+        int i = 0, j = len - 1;
+        while ( i < j ) {
+            if ( s[i] != s[j] ) {
+                return ( isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1) );
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
 };
-
-// 以下解法不能通过所有测试
-// 注意特例 tmpHead == tail && head == tmpTail
-// 如 string s = "aguokepatgbnvfq mgml[c] upuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuc [u]lmgm qfvnbgtapekouga";
-/*
-class FalseSolution {
-public:
-	bool validPalindrome(string s) {
-		int len = s.size();
-
-		if( len <= 1 ){
-			return true;
-		}
-
-		int halflen = len / 2;
-		char head = s[0], tail = s[len-1];
-		bool hasDel = false;
-		int i = 0, j = len - 1;
-		while( i < j ){
-			head = s[i];
-			tail = s[j];
-			if( head != tail ){
-				if( hasDel == false ){
-					hasDel = true;
-					int tmpHead = s[i + 1];
-					int tmpTail = s[j - 1];
-					if( tmpHead == tail ){
-						i++;
-					}else if( head == tmpTail ){
-						j--;
-					}else{
-						return false;
-					}
-				}else{
-					return false;
-				}
-			}
-			i++;
-			j--;
-		}
-		return true;
-	}
-};
-*/
