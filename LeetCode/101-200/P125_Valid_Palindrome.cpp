72441e3aa2f830a0b055f21228d91b12e9971ee8
Steve <ljjdbd123@hotmail.com>
2019-10-27 12:33:33 +0000

    update LeetCode P125

--- LeetCode/101-200/P125_Valid_Palindrome.cpp
+++ LeetCode/101-200/P125_Valid_Palindrome.cpp
@@ -1,67 +1,75 @@
 /*
- * https://leetcode.com/problems/valid-palindrome/description/
- * 判断给定字符串是否为回文字符串
- * 同类题目 P125
- * 
- * 
- * 思路: 先对输入的字符串进行过滤
- * 构造新的字符串, 只留下原字符串中的 alphanum, 并且统一变成小写
- * 再检查新的字符串是否回文
+https://leetcode.com/problems/valid-palindrome/description/?goto=old
+判断给定字符串是否为回文字符串(仅考虑alphanumeric，且忽略大小写)
+同类题目 P125, P680
+
+思路: 先对输入的字符串进行过滤
+构造新的字符串, 只留下原字符串中的 alphanum, 并且统一变成小写
+再检查新的字符串是否回文
 */
 
-// #include <iostream>
-// #include <vector>
-// #include <algorithm>
-// using namespace std;
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
 
 // io加速
-// static const auto io_speed_up = []() {
-// 	std::ios::sync_with_stdio(false);
-// 	cin.tie(0);
-// 	return 0;
-// } ();
+static const auto io_speed_up = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);
+    return 0;
+} ();
 
 class Solution {
 public:
-	bool isPalindrome(string s) {
-		if( s.size() == 0 ){
-			return true;
-		}
+    bool isPalindrome(string s) {
+        int len = s.size();
+        if( len <= 1 ){
+            return true;
+        }
 
-		vector<char> v;
-		for(auto ch : s){
-			if( isalpha(ch) ){
-				v.push_back( tolower(ch) );
-			}else if( isdigit(ch) ){
-				v.push_back( ch );
-			}else{
-				// continue;
-			}
-		}
+        int i = 0, j = len - 1;
+        char ch1, ch2;
+        while ( i < j ) {
+            while ( i < j ) {
+                ch1 = s[i];
+                if ( isalpha(ch1) || isdigit(ch1) ){
+                    break;
+                }
+                else {
+                    i++;
+                }
+            }
+            while ( i < j ) {
+                ch2 = s[j];
+                if ( isalpha(ch2) || isdigit(ch2) ) {
+                    break;
+                }
+                else {
+                    j--;
+                }
+            }
 
-		int len = v.size();
-		int half = len / 2;
-		bool ans = true;
-		for( int i = 0; i < half; i++ ){
-			if( v[i] != v[len-1-i] ){
-				ans = false;
-			}
-		}
-		return ans;
-	}
+            if ( i < j && tolower(ch1) != tolower(ch2) ) {
+                return false;
+            }
+            else {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
 };
 
-// int main(){
-// 	ios::sync_with_stdio(false);
-// 	cin.tie(0);
+int main(){
+    Solution s;
 
-// 	Solution s;
+    string s1 = "A man, a plan, a canal: Panama";
+    cout<<s.isPalindrome(s1)<<'\n';  // 1
 
-// 	string s1 = "A man, a plan, a canal: Panama";
-// 	cout<<s.isPalindrome(s1)<<'\n';	// 1
+    string s2 = "race a car";
+    cout<<s.isPalindrome(s2)<<'\n';  // 0
 
-// 	string s2 = "race a car";
-// 	cout<<s.isPalindrome(s2)<<'\n';	// 0
-
-// 	return 0;
-// }
+    return 0;
+}
