ae94fe76e7a7d7c865301809b646b81a384f0471
Steve <ljjdbd123@hotmail.com>
2019-10-30 12:33:33 +0000

    update LeetCode P206

--- LeetCode/201-300/P206_Reverse_Linked_List.cpp
+++ LeetCode/201-300/P206_Reverse_Linked_List.cpp
@@ -1,7 +1,7 @@
 /*
 https://leetcode.com/problems/reverse-linked-list/description/
 逆转链表
-同类题目 P206
+同类题目 P206, P092
 
 本题采用【头插法】 逆转链表, 你也可以用 【栈】 或者其他方法来逆转链表
 */
@@ -11,49 +11,55 @@ using namespace std;
 
 // io加速
 static const auto io_speed_up = []() {
-	std::ios::sync_with_stdio(false);
-	cin.tie(0);
-	return 0;
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);
+    return 0;
 } ();
 
 struct ListNode {
-	int val;
-	ListNode * next;
-	ListNode(int x) : val(x), next(NULL) {}
+    int val;
+    ListNode * next;
+    ListNode(int x) : val(x), next(NULL) {}
 };
 
 class Solution {
 public:
-	ListNode* reverseList(ListNode* head) {
-		if( head == NULL ){
-			return head;
-		}
-		ListNode * p = head->next;
-		head->next = NULL;
-		while( p != NULL ){
-			ListNode * tmp = p->next;
-			p->next = head;
-			head = p;
-			p = tmp;
-		}
-		return head;
-	}
+    ListNode* reverseList(ListNode* head) {
+        if( head == NULL || head->next == NULL ){
+            return head;
+        }
+
+        ListNode * dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        ListNode * p = head->next;
+        head->next = NULL;
+        while ( p != NULL ) {
+            ListNode * tmp = p->next;
+            p->next = dummyHead->next;
+            dummyHead->next = p;
+            p = tmp;
+        }
+        head = dummyHead->next;
+        delete dummyHead;
+
+        return head;
+    }
 };
 
 int main(){
-	Solution s;
+    Solution s;
 
-	ListNode * head = NULL;
-	s.reverseList(head);
+    ListNode * head = NULL;
+    s.reverseList(head);
 
-	head = new ListNode(1);
-	s.reverseList(head);
+    head = new ListNode(1);
+    s.reverseList(head);
 
-	head->next = new ListNode(2);
-	head->next->next = new ListNode(3);
-	head->next->next->next = new ListNode(4);
-	head->next->next->next->next = new ListNode(5);
-	s.reverseList(head);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+    s.reverseList(head);
 
-	return 0;
+    return 0;
 }
