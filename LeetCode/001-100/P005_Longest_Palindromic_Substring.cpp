2bbbfc6a0e5efcf5753eeaf764d6f341eefdbe2d
FrozenMap <ljjdbd123@hotmail.com>
2020-01-12 12:33:33 +0000

    update Cpp Solution of LeetCode P005

--- LeetCode/001-100/P005_Longest_Palindromic_Substring.cpp
+++ LeetCode/001-100/P005_Longest_Palindromic_Substring.cpp
@@ -6,7 +6,7 @@ https://leetcode.com/problems/longest-palindromic-substring/description/
 参考思路
 https://leetcode.com/problems/longest-palindromic-substring/solution/
 
-从中间向外延伸, expand_from_center
+从中间向外延伸, expandFromCenter
 */
 
 
@@ -15,50 +15,54 @@ https://leetcode.com/problems/longest-palindromic-substring/solution/
 
 class Solution {
 public:
-	int expand_from_center(const string & s, int left, int right, int size){
-		while( left >= 0 && right < size ){
-			if( s[left] != s[right] ){
-				break;
-			}
-			left--;
-			right++;
-		}
-		return right - left - 1;
-	}
+    int getPalindLength( const string & s, int left, int right, int rightBound ) {
+        // Expand from center
+        while ( left >= 0 && right < rightBound ) {
+            if( s[left] != s[right] ){
+                break;
+            }
+            left--;
+            right++;
+        }
+        int length = (right - 1) - (left + 1) + 1;
+        return length;
+    }
 
-	string longestPalindrome(string s) {
-		string ans = "";
-		int size = s.size();
-		int maxlen = 0;
-		int maxstart = 0;
-		if( size > 0 ){
-			for( int i = 0; i < size; i++ ){
-				// int start = 0, end = 0;
-				int len1 = expand_from_center(s, i, i, size);
-				int len2 = expand_from_center(s, i, i + 1, size);
-				if( len1 > len2 ){
-					if( len1 > maxlen ){
-						maxlen = len1;
-						maxstart = i - len1/2;
-					}
-				}
-				else{
-					if( len2 > maxlen ){
-						maxlen = len2;
-						maxstart = i - len2/2 + 1;
-					}
-				}
-			}
-			ans = s.substr(maxstart, maxlen);
-		}
-		return ans;
-	}
+    string longestPalindrome(string s) {
+        string ans = "";
+        int size = s.size();
+        if ( size < 1 ) {
+            return ans;
+        }
+        int maxlen = 0;
+        int start = 0;
+    
+        for ( int i = 0; i < size; i++ ) {
+            int lenOdd = getPalindLength(s, i, i, size);
+            int lenEven = getPalindLength(s, i, i + 1, size);
+            if ( lenOdd > lenEven ) {
+                if( lenOdd > maxlen ) {
+                    maxlen = lenOdd;
+                    start = i - lenOdd / 2;
+                }
+            }
+            else {
+                if( lenEven > maxlen ) {
+                    maxlen = lenEven;
+                    start = i - lenEven / 2 + 1;
+                }
+            }
+        }
+        ans = s.substr(start, maxlen);
+
+        return ans;
+    }
 };
 
 
 int main(){
-	Solution s;
-	s.longestPalindrome("cbbd");	// bb
-	s.longestPalindrome("ababa");	// aba, bab
-	return 0;
+    Solution s;
+    s.longestPalindrome("cbbd");    // bb
+    s.longestPalindrome("ababa");   // aba, bab
+    return 0;
 }
